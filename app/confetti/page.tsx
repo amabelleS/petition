f03fd41d59e2d2979c6e496e52d7d9ca66821933
amabelleS <amabelletr@gmail.com>
2023-11-24 01:29:17 +0200

    hamas confetti

--- app/confetti/page.tsx
+++ app/confetti/page.tsx
@@ -1,6 +1,9 @@
+import PracticlesBG from "../_components/particles/PracticlesBG";
+
 export default function ConfettiPage() {
     return (
         <div className="p-6">
+            <PracticlesBG />
             <h2 className="custom-header">Confetti Page</h2>
             <p>Your personal data will be forwarded to the terror department in the F.B.I.</p>
         </div>
