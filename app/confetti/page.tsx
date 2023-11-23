af879c329269f73aff31bd108298e695c00c34cd
amabelleS <amabelletr@gmail.com>
2023-11-24 01:36:05 +0200

    deleted h1 in confetti page

--- app/confetti/page.tsx
+++ app/confetti/page.tsx
@@ -2,10 +2,10 @@ import PracticlesBG from "../_components/particles/PracticlesBG";
 
 export default function ConfettiPage() {
     return (
-        <div className="p-6">
+        <main className="grid h-screen place-items-center p-4">
             <PracticlesBG />
-            <h2 className="custom-header">Confetti Page</h2>
+            {/* <h2 className="custom-header">Confetti Page</h2> */}
             <p>Your personal data will be forwarded to the terror department in the F.B.I.</p>
-        </div>
+        </main>
     )
   }
