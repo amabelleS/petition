9398b24bd2dc9d6ce08c53745325a69c442a1db3
amabelleS <amabelletr@gmail.com>
2023-11-23 05:22:13 +0200

    spelling

--- app/confetti/page.tsx
+++ app/confetti/page.tsx
@@ -2,7 +2,7 @@ export default function ConfettiPage() {
     return (
         <div className="p-6">
             <h2 className="custom-header">Confetti Page</h2>
-            <p>Your personal data will be forworded to the terror department in the F.B.I.</p>
+            <p>Your personal data will be forwarded to the terror department in the F.B.I.</p>
         </div>
     )
   }
